368f1e9694c9003c12b4fe38881a4783be9a4943
Christian Mello <christian@mello.computer>
2017-11-28 22:23:40 -0800

    Added node class and visited field

--- Project/tsp_christofides.py
+++ Project/tsp_christofides.py
@@ -8,6 +8,18 @@ def calc_distance(x_coord1, y_coord1, x_coord2, y_coord2):
     distance = math.sqrt((x_coord2-x_coord1)**2+(y_coord2-y_coord1)**2)
     return distance
 
+def get_mst(graph):
+    """TODO: Gets the minimum spanning tree of a graph using Prim's algorithm"""
+    graph.print_nodes()
+
+class Node(object):
+    """Class that defines a node that will be in a graph"""
+    def __init__(self, x_coord, y_coord):
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.distances = []
+        self.visited = False
+
 class Graph(object):
     """Class that defines a graph for TSP problem"""
     def __init__(self):
@@ -15,18 +27,24 @@ class Graph(object):
 
     def add_node(self, x_coord, y_coord):
         """Adds a node to the graph"""
-        self.nodes.append([x_coord, y_coord, []])
+        new_node = Node(x_coord, y_coord)
+        self.nodes.append(new_node)
 
     def populate_distances(self):
         """Adds distances to every other node on each node of the graph"""
         for i in range(0, len(self.nodes)):
             for j in range(0, len(self.nodes)):
-                x_coord1 = self.nodes[i][0]
-                y_coord1 = self.nodes[i][1]
-                x_coord2 = self.nodes[j][0]
-                y_coord2 = self.nodes[j][1]
+                x_coord1 = self.nodes[i].x_coord
+                y_coord1 = self.nodes[i].y_coord
+                x_coord2 = self.nodes[j].x_coord
+                y_coord2 = self.nodes[j].y_coord
                 distance = calc_distance(x_coord1, y_coord1, x_coord2, y_coord2)
-                self.nodes[i][2].append(distance)
+                self.nodes[i].distances.append(distance)
+
+    def clear_visited_nodes(self):
+        """Resets all visited nodes to False"""
+        for node in self.nodes:
+            node.visited = False
 
     def print_nodes(self):
         """Show all of the nodes of the graph"""
