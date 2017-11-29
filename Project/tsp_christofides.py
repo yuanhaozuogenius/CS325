6235d1dddc710188049158174c4de4db26ef6748
Christian Mello <christian@mello.computer>
2017-11-28 21:27:02 -0800

    Renamed file, added node distance calculation

---
+++ Project/tsp_christofides.py
@@ -0,0 +1,52 @@
+import math
+
+def calc_distance(x_coord1, y_coord1, x_coord2, y_coord2):
+    distance = math.sqrt((x_coord2-x_coord1)**2+(y_coord2-y_coord1)**2)
+    return distance
+
+class Graph(object):
+    def __init__(self):
+        self.nodes = []
+
+    def add_node(self, x_coord, y_coord):
+        self.nodes.append([x_coord, y_coord, []])
+
+    def populate_distances(self):
+        for i in range(0, len(self.nodes)):
+            for j in range(0, len(self.nodes)):
+                x_coord1 = self.nodes[i][0]
+                y_coord1 = self.nodes[i][1]
+                x_coord2 = self.nodes[j][0]
+                y_coord2 = self.nodes[j][1]
+                distance = calc_distance(x_coord1, y_coord1, x_coord2, y_coord2)
+                self.nodes[i][2].append(distance)
+
+    def print_nodes(self):
+        for node in self.nodes:
+            print(node)
+
+def main():
+    graph = Graph()
+
+    data_file = open("test-input-1.txt", "r")
+    out_file = open("test-input-1-solutions", "w")
+
+    for line in data_file:
+        line_array = list(map(int, line.split()))
+
+        coords = line_array[1:]
+        x_coord = coords[0]
+        y_coord = coords[1]
+
+        graph.add_node(x_coord, y_coord)
+
+        out_file.write('\n')
+
+    graph.populate_distances()
+    graph.print_nodes()
+
+    data_file.close()
+    out_file.close()
+
+if __name__ == "__main__":
+    main()
